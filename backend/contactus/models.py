5bbe38b6fc3ada9e54aa727f06549e7fe19ccf25
Khaledismail320 <khaledamrismail@gmail.com>
2024-09-10 04:35:28 +0300

    contactus done

--- backend/contactus/models.py
+++ backend/contactus/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Messages(models.Model):
+    name = models.TextField()
+    email = models.TextField()
+    subject = models.TextField(null=True)
+    inquiryType = models.TextField()
+    Message = models.TextField()
+    
--- backend/contactus/views.py
+++ backend/contactus/views.py
@@ -1,3 +1,72 @@
 from django.shortcuts import render
-
+import requests
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponseNotAllowed
+from django.core.exceptions import ObjectDoesNotExist 
 # Create your views here.
+from .models import Messages
+import json
+
+@csrf_exempt
+def newMessage(request):
+    data=json.load(request)
+    name=data['name']
+    email=data['email']
+    subject=data['subject']
+    inquiryType=data['inquiryType']
+    Message=data['Message']
+    # Create a new message
+    try:
+        newMessage = Messages(name=name,email=email,subject=subject,inquiryType=inquiryType,Message=Message)
+        newMessage.save()
+        
+        response = {
+            "message": "User created successfully",
+            }
+
+        return HttpResponse( json.dumps(response),  status=200)
+    except Exception as e:
+        response = {
+        "error": str(e),
+        }
+        return HttpResponse( json.dumps(response),  status=400)
+    
+    
+@csrf_exempt 
+def getAllMessages(request):
+    try:
+        messages = Messages.objects.all()
+        response = {
+            "messages": list(messages.values())
+        }
+        return JsonResponse(response, status=200)
+    except Exception as e:
+        response = {
+        "error": str(e),
+        }
+        return HttpResponse( json.dumps(response),  status=400)
+    
+
+@csrf_exempt
+def deleteMessage(request):
+    try:
+        data = json.load(request) 
+        id=data['id']
+        
+        message = Messages.objects.get(id=id)
+        message.delete()
+        response = {
+            "message": "Message deleted successfully",
+            }
+        return HttpResponse( json.dumps(response),  status=200)
+    except ObjectDoesNotExist:
+        response = {
+            "error": "Message not found",
+            }
+        return HttpResponse( json.dumps(response),  status=400)
+    except Exception as e:
+        response = {
+            "error": str(e),
+            }
+        return HttpResponse( json.dumps(response),  status=400)     
